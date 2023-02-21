254105befc9b1462a0e2471c201f9e2a5ef3c7c3
rama4zis <rama.4ditya3@gmail.com>
2023-02-21 18:57:23 +0700

    add current banner

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,71 +1,111 @@
+import currentBanner from "./../assets/images/currentBanner.png";
+
 const HomePage = () => {
   return (
     <>
       <div className="lg:columns-3 md:columns-2 gap-6 mt-8 min-h-screen">
-        
         {/* Greating  */}
         <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
-            <p className="text-2xl text-white font-bold my-4">Welcome to ArisuArchive 👋</p>
-            <p className="text-white">Blue Archive community helper and guide, helps you plan what to farm with an easy to use planner, and also provides you with a list of all the characters in the game.</p>
+          <p className="text-2xl text-white font-bold my-4">
+            Welcome to ArisuArchive 👋
+          </p>
+          <p className="text-white">
+            Blue Archive community helper and guide, helps you plan what to farm
+            with an easy to use planner, and also provides you with a list of
+            all the characters in the game.
+          </p>
         </div>
 
         {/* Current Event */}
         <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
-            <p className="text-2xl text-white mb-2">Current Event</p>
-            <div className="flex flex-col gap-1">
-                <div className="p-2 rounded-xl flex bg-sky-500 text-black">
-                    <span>Overflowing Mastery</span>
-                    <span className="ml-auto">2021-08-31</span>
-                </div>
-
-                <div className="p-2 rounded-xl flex bg-purple-500 text-black">
-                    <span>Overflowing Mastery</span>
-                    <span className="ml-auto">2021-08-31</span>
-                </div>
-
-                <div className="p-2 rounded-xl flex bg-cyan-500 text-black">
-                    <span>Overflowing Mastery</span>
-                    <span className="ml-auto">2021-08-31</span>
-                </div>
-
-                <div className="p-2 rounded-xl flex bg-amber-500 text-black">
-                    <span>Overflowing Mastery</span>
-                    <span className="ml-auto">2021-08-31</span>
-                </div>
-
-                <div className="p-2 rounded-xl flex bg-emerald-500 text-black">
-                    <span>Overflowing Mastery</span>
-                    <span className="ml-auto">2021-08-31</span>
-                </div>
+          <p className="text-2xl text-white mb-2">Current Event</p>
+          <div className="flex flex-col gap-1">
+            <div className="p-2 rounded-xl flex bg-sky-500 text-black">
+              <span>Overflowing Mastery</span>
+              <span className="ml-auto">2021-08-31</span>
             </div>
+
+            <div className="p-2 rounded-xl flex bg-purple-500 text-black">
+              <span>Overflowing Mastery</span>
+              <span className="ml-auto">2021-08-31</span>
+            </div>
+
+            <div className="p-2 rounded-xl flex bg-cyan-500 text-black">
+              <span>Overflowing Mastery</span>
+              <span className="ml-auto">2021-08-31</span>
+            </div>
+
+            <div className="p-2 rounded-xl flex bg-amber-500 text-black">
+              <span>Overflowing Mastery</span>
+              <span className="ml-auto">2021-08-31</span>
+            </div>
+
+            <div className="p-2 rounded-xl flex bg-emerald-500 text-black">
+              <span>Overflowing Mastery</span>
+              <span className="ml-auto">2021-08-31</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Current Banner  */}
+        <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
+          <div className="relative mb-4">
+            <img src={currentBanner} alt="current banner" />
+            <div className="absolute bottom-0 left-0 flex justify-between w-full p-2">
+              <div className="text-slate-800 text-xs font-light bg-slate-100 p-1 rounded-md">Arisu</div>
+              <div className="text-slate-800 text-xs font-light bg-slate-100 p-1 rounded-md">Aru</div>
+              <div className="text-slate-800 text-xs font-light bg-slate-100 p-1 rounded-md">Shiroko (Riding)</div>
+              <div className="text-slate-800 text-xs font-light bg-slate-100 p-1 rounded-md">Saori</div>
+            </div>
+          </div>
+          <p className="text-white">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni
+            debitis quae veritatis aspernatur voluptatum nostrum voluptates
+            culpa ut, animi minima, quasi minus temporibus necessitatibus
+            voluptate est dolor tenetur, inventore modi molestiae fuga nemo qui
+            doloribus nisi. Quibusdam reprehenderit officiis molestias. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eaque
+            sequi.
+          </p>
         </div>
 
         <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
-            <p className="text-2xl text-white">Hello Woaarld</p>
-            <p className="text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni debitis quae veritatis aspernatur voluptatum nostrum voluptates culpa ut, animi minima, quasi minus temporibus necessitatibus voluptate est dolor tenetur, inventore modi molestiae fuga nemo qui doloribus nisi. Quibusdam reprehenderit officiis molestias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, eaque sequi.</p>
-        </div>
-
-
-        <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
-            <p className="text-2xl text-white">Hello World</p>
-            <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, molestiae?</p>
+          <p className="text-2xl text-white">Hello World</p>
+          <p className="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+            molestiae?
+          </p>
         </div>
 
         <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
-            <p className="text-2xl text-white">Hello World</p>
-            <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, molestiae?</p>
+          <p className="text-2xl text-white">Hello World</p>
+          <p className="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+            molestiae?
+          </p>
         </div>
 
         <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
-            <p className="text-2xl text-white">Hello World</p>
-            <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nihil praesentium quaerat atque ducimus repellat perferendis nemo illo recusandae, repellendus vel porro ea eum fuga repudiandae aut molestias in consequuntur? Rerum minus iure, nemo nulla aliquid animi nostrum. Repudiandae, nulla. Fuga temporibus corrupti voluptatum dolorum.</p>
+          <p className="text-2xl text-white">Hello World</p>
+          <p className="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            nihil praesentium quaerat atque ducimus repellat perferendis nemo
+            illo recusandae, repellendus vel porro ea eum fuga repudiandae aut
+            molestias in consequuntur? Rerum minus iure, nemo nulla aliquid
+            animi nostrum. Repudiandae, nulla. Fuga temporibus corrupti
+            voluptatum dolorum.
+          </p>
         </div>
 
         <div className="break-inside-avoid bg-slate-600 rounded-md p-4 mb-6">
-            <p className="text-2xl text-white">Hello World</p>
-            <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam magni eligendi, repellendus nesciunt distinctio numquam libero vero maxime voluptate minus voluptatum optio non tempora unde eveniet, dolorem iusto pariatur expedita quas qui fugiat architecto alias.</p>
+          <p className="text-2xl text-white">Hello World</p>
+          <p className="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam magni
+            eligendi, repellendus nesciunt distinctio numquam libero vero maxime
+            voluptate minus voluptatum optio non tempora unde eveniet, dolorem
+            iusto pariatur expedita quas qui fugiat architecto alias.
+          </p>
         </div>
-
       </div>
     </>
   );
